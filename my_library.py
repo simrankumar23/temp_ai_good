7ba934b6e4e8cbbe432c89be2390d9c78abd7412
simrankumar23 <103154783+simrankumar23@users.noreply.github.com>
2022-05-12 12:03:13 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -1,3 +1,8 @@
 def hello(name):
   return f'Hi {name}'
 
+def cond_prob(table, evidence, evidence_value, target, target_value):
+  t_subset = up_table_subset(table, target, 'equals', target_value)
+  e_list = up_get_column(t_subset, evidence)
+  p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
+  return p_b_a + .01  #Laplace smoothing factor
